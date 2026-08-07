87207f4f5381ade57e4f1d028334c853caa57aae
Robert Mave E. Veloria <velorir1@universalleaf.com>
2026-08-07 16:11:41 +0800

    000111-0726-007 - Initial

--- Controllers/TrainingProgramAndCourseController.cs
+++ Controllers/TrainingProgramAndCourseController.cs
@@ -335,16 +335,22 @@ namespace TRS.Controllers
             {
                 
                 TrainingCourse trainingCourse =  await _trainingCourseService.GetTrainingCourseByCode(code);
+                List<CourseCategory> courseCategory = await _trainingProgramService.GetCourseCategoriesList();
                 TrainingProgram programDetails = null;
+                CourseCategory category = null;
                 if(trainingCourse != null)
-                programDetails = await _trainingProgramService.GetTrainingProgramByCode(trainingCourse.Program.ProgramCode);
+                {
+                    programDetails = await _trainingProgramService.GetTrainingProgramByCode(trainingCourse.Program.ProgramCode);
+                    category = courseCategory.Where(c => c.CategoryCode == trainingCourse.CategoryCode).FirstOrDefault();
+                }
 
                 List<JobClass> jobclassList =  await _jobclassService.GetHRJobClassList();
                 TrainingProgramAndCourseViewModel trainingProgramAndCourseViewModel = new TrainingProgramAndCourseViewModel(){
                     FormControl = new FormControlModel(control),
                     TrainingProgram = programDetails,
                     JobClasses = jobclassList,
-                    TrainingCourse = trainingCourse
+                    TrainingCourse = trainingCourse,
+                    CourseCategory = category
                 };
 
                 return PartialView("~/Views/TrainingProgramAndCourse/_TrainingCourseDetails.cshtml", trainingProgramAndCourseViewModel);                
@@ -462,6 +468,21 @@ namespace TRS.Controllers
 
             // Return success response
             return Ok();
+        }
+
+        public async Task<JsonResult> GetCourseCategoryList()
+        {
+            List<CourseCategory> _list = new()
+            {
+                new CourseCategory
+                {
+                    CategoryCode = "",
+                    CategoryTitle = "",
+                }
+            };
+            _list.AddRange(await _trainingProgramService.GetCourseCategoriesList());
+
+            return Json(_list.Where(w => w.Status == true).ToList());
         }
 #endregion
 
---
+++ Migrations/20260807053938_mCourseCategory.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TRS.Migrations
+{
+    /// <inheritdoc />
+    public partial class mCourseCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormAccess");
+
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+
+            migrationBuilder.CreateTable(
+                name: "mCourseCategory",
+                schema: "TRS",
+                columns: table => new
+                {
+                    CategoryCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RecordNo = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedByComputerUsed = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedByComputerUsed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_mCourseCategory", x => x.CategoryCode);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "mCourseCategory",
+                schema: "TRS");
+
+            migrationBuilder.CreateTable(
+                name: "FormAccess",
+                columns: table => new
+                {
+                    AccessType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccessibleDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Controller = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrentVersion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DevInfo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DevInitials = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeNo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FormID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FormName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubMenuID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubMenuName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    BranchID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BranchName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DepartmentID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayPic = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmpID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HREmployeeStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PositionName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SectionID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SectionName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserID = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+    }
+}
